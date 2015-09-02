0d49e3dc1aaebc7c5af7333b84781580b6c3a7c9
balibou <benjamin.cherion@gmail.com>
2015-09-02 20:01:19 +0200

    commit data to check

--- client/views/messages.js
+++ client/views/messages.js
@@ -1,5 +1,18 @@
 Template.messages.helpers({
   msgs: function() {
     return Messages.find({roomId: this._id});
-  }
+  },
+});
+
+Template.messages.onRendered(function () {
+  var instance = this;
+  var query = Messages.find({roomId: this._id});
+  
+  instance.autorun(function () {
+    query.observeChanges({
+      added: function () {
+        console.log('test');
+      }
+    });
+  });
 });
